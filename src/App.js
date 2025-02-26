b6d44c06a2d6841070019d715aac1e7264f85e8b
Ashwini Kumar <ashwiniyadav1702@gmail.com>
2025-02-27 01:54:05 +0530

    initial change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignIn from "./components/Auth/SignIn";
 import SignUp from "./components/Auth/Signup";
 
 function App() {
+  console.log("hello")
   return (
     <div className="App">
       <SignIn/>
